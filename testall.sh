f084514e12f77cda76eddaa1c6c29d07fd3322b1
Michael Zhou <michael.zhou@gmail.com>
2013-09-18 20:45:07 -0400

    Added shell script to running all tests in one shot.

---
+++ testall.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+cd ~/logging-profiling
+mvn clean
+
+cd ~/logging-profiling/log4jv1-native
+mvn install
+echo ""
+echo ""
+echo "=============================="
+echo " number of lines in log files"
+wc -l *.out
+rm *.out
+echo "=============================="
+echo ""
+echo ""
+
+cd ~/logging-profiling/log4jv1-slf4j
+mvn install
+echo ""
+echo ""
+echo "=============================="
+echo "number of lines in log files"
+wc -l *.out
+rm *.out
+echo "=============================="
+echo ""
+echo ""
+
+cd ~/logging-profiling/log4jv2-native
+mvn install
+echo ""
+echo ""
+echo "=============================="
+echo "number of lines in log files"
+wc -l *.out
+rm *.out
+echo "=============================="
+echo ""
+echo ""
+
+cd ~/logging-profiling/log4jv2-slf4j
+mvn install
+echo ""
+echo ""
+echo "=============================="
+echo "number of lines in log files"
+wc -l *.out
+rm *.out
+echo "=============================="
+echo ""
+echo ""
+
+cd ~/logging-profiling/logback
+mvn install
+echo ""
+echo ""
+echo "=============================="
+echo "number of lines in log files"
+wc -l *.out
+rm *.out
+echo "=============================="
+echo ""
+echo ""
