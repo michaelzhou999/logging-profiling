c4c61220fb384c797d26a88a55eaeb8c3d44990c
Michael Zhou <michael.zhou@gmail.com>
2013-09-20 13:50:40 -0400

    Move everything into junit tests.	Deleted mojo plugin from all pom files.

--- common/src/main/java/com/test/logging/common/Util.java
+++ common/src/main/java/com/test/logging/common/Util.java
@@ -2,10 +2,10 @@ package com.test.logging.common;
 
 public class Util {
     public static void sleepForIOCatchup() {
-        // Wait for 20 seconds for I/O to catch up and closing of async appenders
-        System.out.println("Waiting for 20 seconds for I/O to catch up and proper closing of async appenders.");
+        // Wait for I/O to catch up and close async appenders
+        System.out.println("Waiting 10 seconds for I/O to catch up and properly close async appenders.");
         try {
-            Thread.sleep(20000);
+            Thread.sleep(10000);
         } catch (InterruptedException ie) {
             System.err.println("Interrupted while waiting for I/O to catch up.");
         }
