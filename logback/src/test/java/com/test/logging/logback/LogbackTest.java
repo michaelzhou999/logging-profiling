3800800c2eb086d52cc0f7b413a08de0449f98a7
Michael Zhou <michael.zhou@gmail.com>
2013-09-20 15:00:44 -0400

    Use sleep method in common.Util for logback test.

--- logback/src/test/java/com/test/logging/logback/LogbackTest.java
+++ logback/src/test/java/com/test/logging/logback/LogbackTest.java
@@ -13,6 +13,7 @@ import ch.qos.logback.classic.LoggerContext;
 import com.test.logging.common.LoggingTest;
 import com.test.logging.common.ProfilerOptions;
 import com.test.logging.common.TestFactoryType;
+import static com.test.logging.common.Util.sleepForIOCatchup;
 
 /** Logback logger test */
 public class LogbackTest {
@@ -62,13 +63,8 @@ public class LogbackTest {
         for (TestFactoryType t : allTypes) {
             new LoggingTest<Logger>(t.toString() + "-Async", new TestFactory(t, asyncLogger), opts).run();
         }
-        // Wait for 20 seconds for I/O to catch up and closing of async appenders
-        System.out.println("Waiting for 20 seconds for I/O to catch up and proper closing of async appenders.");
-        try {
-            Thread.sleep(20000);
-        } catch (InterruptedException ie) {
-            System.err.println("Interrupted while waiting for I/O to catch up.");
-        }
+
+        sleepForIOCatchup();
     }
 
 }
