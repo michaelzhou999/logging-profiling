e64e8a5d5014c09c9ce135bb342257ffdb384102
Michael Zhou <michael.zhou@gmail.com>
2013-09-24 12:23:29 -0400

    Add test cases for Java Util Logging (JUL) to the benchmarking tool.

---
+++ java-util-logging/src/test/java/com/test/logging/jul/JulTest.java
@@ -0,0 +1,65 @@
+package com.test.logging.jul;
+
+import java.util.logging.Logger;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.test.logging.common.LoggingTest;
+import com.test.logging.common.ProfilerOptions;
+import com.test.logging.common.TestFactoryType;
+import static com.test.logging.common.Util.sleepForIOCatchup;
+
+/** JUL logger test */
+public class JulTest {
+
+    /** Profiler options */
+    private static final ProfilerOptions opts = new ProfilerOptions("Java Util Logging Profiler");
+
+    /** Different types of tests */
+    private static final TestFactoryType[] allTypes = TestFactory.getSupportedTypes();
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testSyncFileLogger() {
+        // Synchronous file logger
+        System.out.println("(((((((((((((   SYNC FILE LOGGER   ))))))))))))))))");
+        Logger logger = Logger.getLogger("SyncFileLogger");
+        // Iterate through all unit work types and execute test scenarios
+        for (TestFactoryType t : allTypes) {
+            new LoggingTest<Logger>(t.toString() + "-Sync", new TestFactory(t, logger), opts).run();
+        }
+    }
+
+    //@Test
+    public void testAsyncFileLogger() {
+        // Asynchronous file logger
+        System.out.println("(((((((((((((   ASYNC FILE LOGGER   ))))))))))))))))");
+        Logger asyncLogger = Logger.getLogger("AsyncFileLogger");
+        // Iterate through all unit work types and execute test scenarios
+        for (TestFactoryType t : allTypes) {
+            new LoggingTest<Logger>(t.toString() + "-Async", new TestFactory(t, asyncLogger), opts).run();
+        }
+
+        sleepForIOCatchup();
+    }
+
+}
---
+++ java-util-logging/src/test/java/com/test/logging/jul/TestFactory.java
@@ -0,0 +1,145 @@
+package com.test.logging.jul;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.test.logging.common.BaseTestScenarioFactory;
+import com.test.logging.common.TestFactoryType;
+import com.test.logging.common.TestScenario;
+import com.test.logging.common.TestScenarioOptions;
+
+/**
+ * Factory to create units of logging test work to use with JUL APIs.
+ * 
+ * @author Michael.Zhou
+ */
+public class TestFactory extends BaseTestScenarioFactory<Logger> {
+
+    /** Supported types */
+    private static final TestFactoryType[] types = new TestFactoryType[] { TestFactoryType.LITERAL,
+            TestFactoryType.LEVEL_CHECKED_LITERAL, TestFactoryType.CONCAT_STRING,
+            TestFactoryType.JAVA_FORMATTED_STRING };
+
+    /** Type of factory */
+    private TestFactoryType type;
+
+    /** Logger */
+    private Logger logger;
+
+    public TestFactory(TestFactoryType type, Logger logger) {
+        this.type = type;
+        this.logger = logger;
+    }
+
+    public static TestFactoryType[] getSupportedTypes() {
+        return types;
+    }
+
+    /** All unit works created by this factory share the same logger. */
+    public TestScenario<Logger> createTestScenario(TestScenarioOptions options) {
+        switch (this.type) {
+        case LITERAL:
+            return new LiteralLogging(logger, options);
+        case LEVEL_CHECKED_LITERAL:
+            return new LevelCheckedLogging(logger, options);
+        case CONCAT_STRING:
+            return new ConcatStringLogging(logger, options);
+        case JAVA_FORMATTED_STRING:
+            return new JavaFormattedStringLogging(logger, options);
+        default:
+            throw new RuntimeException("Unsupported type of factory");
+        }
+    }
+
+}
+
+/**
+ * Logs a string literal without level check.
+ * 
+ * @author Michael.Zhou
+ */
+class LiteralLogging extends TestScenario<Logger> {
+
+    public LiteralLogging(Logger logger, TestScenarioOptions options) {
+        super(logger, options);
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < options.getNumberOfRepeats(); i++) {
+            logger.info(TestFactory.LOG_MSG);
+        }
+    }
+
+}
+
+/**
+ * Logs a string literal wrapped within level check.
+ * 
+ * @author Michael.Zhou
+ */
+class LevelCheckedLogging extends TestScenario<Logger> {
+
+    public LevelCheckedLogging(Logger logger, TestScenarioOptions options) {
+        super(logger, options);
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < options.getNumberOfRepeats(); i++) {
+            if (logger.isLoggable(Level.INFO)) {
+                logger.info(TestFactory.LOG_MSG);
+            }
+        }
+    }
+
+}
+
+/**
+ * Logs a string concatenated via '+' wrapped within level check.
+ * 
+ * @author Michael.Zhou
+ */
+class ConcatStringLogging extends TestScenario<Logger> {
+
+    public ConcatStringLogging(Logger logger, TestScenarioOptions options) {
+        super(logger, options);
+    }
+
+    @Override
+    public void run() {
+        String varPart1 = TestFactory.VARIABLE_STRING_PART_1;
+        String varPart2 = TestFactory.VARIABLE_STRING_PART_2;
+        for (int i = 0; i < options.getNumberOfRepeats(); i++) {
+            if (logger.isLoggable(Level.INFO)) {
+                logger.info(TestFactory.FIXED_STRING_PART_1 + varPart1 + TestFactory.FIXED_STRING_PART_2 + varPart2
+                        + TestFactory.FIXED_STRING_PART_3);
+            }
+        }
+    }
+
+}
+
+/**
+ * Logs a string via String.format wrapped within level check.
+ * 
+ * @author Michael.Zhou
+ */
+class JavaFormattedStringLogging extends TestScenario<Logger> {
+
+    public JavaFormattedStringLogging(Logger logger, TestScenarioOptions options) {
+        super(logger, options);
+    }
+
+    @Override
+    public void run() {
+        String varPart1 = TestFactory.VARIABLE_STRING_PART_1;
+        String varPart2 = TestFactory.VARIABLE_STRING_PART_2;
+        for (int i = 0; i < options.getNumberOfRepeats(); i++) {
+            if (logger.isLoggable(Level.INFO)) {
+                logger.info(String.format(TestFactory.JAVA_STRING_FORMAT, varPart1, varPart2));
+            }
+        }
+    }
+
+}
